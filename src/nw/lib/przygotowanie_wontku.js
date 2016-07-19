2ea441517345ccd6e4fd062828d7e39c1106d072
oooexec <pocztek.skrytczewski.sec@gmail.com>
2016-07-19 02:54:36 +0200

    Dodane pliki z kodem źródłowym do folderu "lib".

---
+++ src/nw/lib/przygotowanie_wontku.js
@@ -0,0 +1,6 @@
+function pobierz_wpis ( okno, id_wątku ) {
+	let elem	=	(okno.document.getElementsByClassName ( "entry iC" ))[id_wątku.arg];
+	let tmp		=	elem;
+}
+
+exports.pobierz_wpis = pobierz_wpis;
