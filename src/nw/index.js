453c1654f5a7b460cec16c8d185a7cf73dafe9ec
oooexec <pocztek.skrytczewski.sec@gmail.com>
2016-07-19 02:53:58 +0200

    Dodana część plików źródłowych.

---
+++ src/nw/index.js
@@ -0,0 +1,7 @@
+/* Wywołanie metody inicjalizacja uruchamia moduł.
+ * 
+ * 
+ * 
+ */ 
+require ( "./lib/zdjencia_mod.js" ).inicjalizacja();
+//require ( "./lib/zdjencia_mod.js" ).test_oznacz_wpisy_js();
